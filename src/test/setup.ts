403c33d3416a9e4b39943e50cd9a243d0799bb17
Daniel D <d.drizhuk@gmail.com>
2025-09-13 15:16:48 +0200

    Override console for grpc

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -4,6 +4,16 @@ jest.setTimeout(20000);
 
 beforeAll(() => {
   process.env.NEBIUS_LOG = process.env.NEBIUS_LOG || 'none';
+  if (process.env.NEBIUS_LOG_OUTPUT === 'stderr') {
+    console.error = (msg, ...args) => {
+      // Avoid logging empty messages
+      if (msg === undefined || msg === null || (typeof msg === 'string' && msg.trim() === '')) {
+        return;
+      }
+
+      process.stderr.write(require('util').format(msg, ...args) + '\n');
+    };
+  }
 });
 // Ensure env vars modified in tests do not leak across tests
 beforeEach(() => {
